8a7bd55788d335ebe60fd90fad5f3398df8cf66c
leeyoonchae <yoonchae2015@gmail.com>
2024-07-16 19:27:21 +0900

    feat : add postCell, ColorExt

---
+++ sfida/sfida/UI/Home/Component/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  sfida
+//
+//  Created by dgsw8th61 on 7/16/24.
+//
+
+import SwiftUI
+
+struct PostCell : View {
+    let title : String
+    var body: some View {
+        VStack(alignment:.leading,spacing: 7){
+            Image("testImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 140)
+            Text(title)
+                .bold()
+                .font(.system(size: 12))
+        }
+    }
+}
+#Preview{
+    PostCell(title: "탄소줄이기 운동")
+}
---
+++ sfida/sfida/Util/ColorExt.swift
@@ -0,0 +1,36 @@
+//
+//  ColorEx.swift
+//  sfida
+//
+//  Created by dgsw8th61 on 7/16/24.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let gray0 = Color(hex: "#5B5B5B")
+    static let gray1 = Color(hex: "#797979")
+    static let gray2 = Color(hex: "#9F9F9F")
+    static let gray3 = Color(hex: "#C4C4C4")
+    static let Main = Color(hex: "#0094FF")
+    static let BackgroundColor = Color(hex: "#FAFAFA")
+    static let Background = Color(hex: "FCFCFC")
+}
+
+extension Color {
+  init(hex: String) {
+      let scanner = Scanner(string: hex)
+         _ = scanner.scanString("#")
+         
+         var rgb: UInt64 = 0
+         scanner.scanHexInt64(&rgb)
+         
+         let r = Double((rgb >> 16) & 0xFF) / 255.0
+         let g = Double((rgb >>  8) & 0xFF) / 255.0
+         let b = Double((rgb >>  0) & 0xFF) / 255.0
+         self.init(red: r, green: g, blue: b)
+
+      
+  }
+}
