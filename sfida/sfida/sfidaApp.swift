e68a29b159c6e212741d32ad2f1d51c5228c39d2
이윤채 <130755399+leeyoonchae@users.noreply.github.com>
2024-07-16 19:12:56 +0900

    create

---
+++ sfida/sfida/sfidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfidaApp.swift
+//  sfida
+//
+//  Created by dgsw8th61 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct sfidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sfida/sfidaUITests/sfidaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sfidaUITestsLaunchTests.swift
+//  sfidaUITests
+//
+//  Created by dgsw8th61 on 7/16/24.
+//
+
+import XCTest
+
+final class sfidaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
