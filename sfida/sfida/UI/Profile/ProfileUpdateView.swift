ec80be1ecdd25c6abc31210e8faac1e154a68f29
uoto00 <di35180@gmail.com>
2024-07-17 15:25:42 +0900

    feat : add ProfileView

---
+++ sfida/sfida/UI/Profile/ProfileUpdateView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileUpdateView.swift
+//  sfida
+//
+//  Created by 이다경 on 7/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileUpdateView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ProfileUpdateView()
+}
