25b321f6ff068dbfc027ebf8c6dad37112bf6517
uoto00 <di35180@gmail.com>
2024-07-17 10:34:10 +0900

    feat : update SignInView

--- sfida/sfida/UI/Auth/SignInView.swift
+++ sfida/sfida/UI/Auth/SignInView.swift
@@ -34,64 +34,63 @@ struct SignInView: View {
     }
     
     private var BodyView: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                Text("SFIDA. 계정으로 로그인 해주세요")
-                    .font(.title2)
-                TextField("아이디", text: $username)
-                    .autocapitalization(.none)
+        GeometryReader{ geometryReader in
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("SFIDA. 계정으로 로그인 해주세요")
+                        .font(.title2)
+                    TextField("아이디", text: $username)
+                        .autocapitalization(.none)
+                        .padding(.leading)
+                        .frame(width: geometryReader.size.width / 1.15, height: geometryReader.size.width / 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6 )
+                                .stroke(Color.gray2, lineWidth: 1)
+                        )
+                        .onChange(of: username) { newValue in
+                            isTextFieldFilled = !newValue.isEmpty
+                        }
+                    HStack {
+                        if showpassword {
+                            SecureField("비밀번호", text: $password)
+                                .autocapitalization(.none)
+                        } else {
+                            TextField("비밀번호",text: $password)
+                                .autocapitalization(.none)
+                        }
+                        Button(action: {
+                            self.showpassword.toggle()
+                        }, label: {
+                            Image(systemName: showpassword ? "eye.slash" : "eye")
+                                .padding(.trailing)
+                                .foregroundColor(.gray2)
+                        })
+                    }
                     .padding(.leading)
-                    .frame(width: 350, height: 50)
+                    .frame(width: geometryReader.size.width / 1.15, height: geometryReader.size.width / 8)
                     .overlay(
                         RoundedRectangle(cornerRadius: 6 )
-                            .stroke(.red, lineWidth: 1)
+                            .stroke(Color.gray2, lineWidth: 1)
                     )
-                    .onChange(of: username) { newValue in
-                        isTextFieldFilled = !newValue.isEmpty
+                    .onChange(of: password) { newValue in
+                        isPasswordFilled = !newValue.isEmpty
                     }
-                HStack {
-                    if showpassword {
-                        TextField("비밀번호", text: $password)
-                            .autocapitalization(.none)
-                    } else {
-                        SecureField("비밀번호",text: $password)
-                            .autocapitalization(.none)
-                    }
-                    Button(action: {
-                        self.showpassword.toggle()
-                    }, label: {
-                        Image(systemName: showpassword ? "eye" : "eye.slash")
-                            .padding(.trailing)
-                            .foregroundColor(.red)
-                    })
                 }
-                .padding(.leading)
-                .frame(width: 350, height: 50)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6 )
-                        .stroke(.red, lineWidth: 1)
-                )
-                .onChange(of: password) { newValue in
-                    isPasswordFilled = !newValue.isEmpty
+                NavigationLink(destination: SignUpView()) {
+                    Text("로그인")
+                        .frame(width: geometryReader.size.width / 1.2, height: geometryReader.size.height / 8)
+                        .foregroundColor(.white)
+                        .background(isTextFieldFilled && isPasswordFilled ? Color.Main : Color.Main.opacity(0.5))
+                        .cornerRadius(10)
                 }
-                .padding(.top).frame(height: 45)
-                Spacer()
-            }
-            NavigationLink(destination: SignUpView()) {
-                Text("로그인")
-                    .frame(width: 350, height: 50)
-                    .foregroundColor(.white)
-                    .background(isTextFieldFilled && isPasswordFilled ? Color.Main : Color.Main.opacity(0.5))
-                    .cornerRadius(10)
-            }
-            .disabled(!isTextFieldFilled || !isPasswordFilled)
-            NavigationLink(destination: SignUpView()) {
-                Text("SFIDA.에 처음 오셨나요?")
-                    .underline()
-                    .font(.system(size: 13))
+                .disabled(!isTextFieldFilled || !isPasswordFilled)
+                NavigationLink(destination: SignUpView()) {
+                    Text("SFIDA.에 처음 오셨나요?")
+                        .underline()
+                        .font(.system(size: geometryReader.size.width/30))
+                }
             }
         }
-        .padding(.top, 50)
     }
 }
 
